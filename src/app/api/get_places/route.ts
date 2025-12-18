bbd4a594251d38cc8c9e13e4cf6e8e8adf4adcbd
Lorenzo Felici <loryfelix42@gmail.com>
2025-12-18 20:39:24 +0100

    Ho aggiunto nel file get_places/route.ts la condizione che controlla se lo spazio studio  è aperto o chiuso in base agli orari, manca la condizione per controllare se nel giorno in questione lo spazio è a prescindere chiuso.

--- src/app/api/get_places/route.ts
+++ src/app/api/get_places/route.ts
@@ -33,14 +33,24 @@ export async function GET(request: Request) {
       WHERE stato=2
     `);
 
+    const now = new Date();
+    const getDate = now.getDay() - 1;               //Giorno della settimana (0=Lunedì, 6=Domenica)
+    const currentMinutes = now.getHours() * 60 + now.getMinutes();      //Prendi l'orario attuale in minuti 
     const customData = result.rows.map(row => ({
       id: row.id,
       placeName: row.place_name,
       placeLat: row.place_lat,
       placeLong: row.place_long,
-      isClosed: false,
+      placeOpeningHours: row.place_opening_hours,
+      placeClosingHours: row.place_closing_hours,
+      isClosed: row.place_opening_hours !== null && row.place_closing_hours !== null                  
+        ? (currentMinutes <= row.place_opening_hours && currentMinutes > row.place_closing_hours && getDate)     //Manca la condizione che controlla se il giorno in questione è chiuso     
+        : false,
       entryMode: row.entry_mode
     }));
+    //console.log("Giorno:", now.getDay()-1);
+    //console.log("ORARIO ATTUALE IN MINUTI:", currentMinutes);         //Questi erano per controllare se andava bene il calcolo
+    //console.log("POSTO", customData);
 
     return NextResponse.json(customData, {
       status: 200,
